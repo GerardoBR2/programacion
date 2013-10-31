bb697381a8f02db5301b47886e047c89117dd78f
Gerardo <gerardo@B301-13.dai.red>
2013-10-31 17:39:09 +0100

    Cambios

--- Unidad2/src/graficos/Semaforo.java
+++ Unidad2/src/graficos/Semaforo.java
@@ -8,7 +8,7 @@ public class Semaforo {
 		Ellipse verde,ambar,rojo;
 		
 		
-		caja = new Rectangle(10,10,60,100);
+		caja = new Rectangle(10,10,60,110);
 		caja.setColor(new Color(0,0,0));
 		caja.draw();
 		caja.fill();
@@ -18,17 +18,17 @@ public class Semaforo {
 		
 		palo.fill();
 		
-		verde = new Ellipse(32,80,20,20);
+		verde = new Ellipse(30,80,20,20);
 		verde.setColor(new Color(0,200,0));
 		verde.draw();
 		verde.fill();
 		
-		ambar= new Ellipse(32,50,20,20);
+		ambar= new Ellipse(30,50,20,20);
 		ambar.setColor(new Color(255,255,0));
 		ambar.draw();
 		ambar.fill();
 		
-		rojo= new Ellipse(32,20,20,20);
+		rojo= new Ellipse(30,20,20,20);
 		rojo.setColor(new Color(255,0,0));
 		rojo.draw();
 		rojo.fill();
